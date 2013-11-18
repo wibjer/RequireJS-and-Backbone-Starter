66a31e25dc4969c7f0b38f5cb45a2610402702fa
OpenShift guest <johanwibjer@gmail.com>
2013-11-18 11:16:35 -0500

    update

--- app/scripts/views/app.js
+++ app/scripts/views/app.js
@@ -1,7 +1,18 @@
 define(['backbone'], function(Backbone) {
   var App = Backbone.View.extend({
+    el: $('body'),
+    events:{
+        'click button#add' : 'additem'
+    },
     initialize: function() {
-      console.log( 'Wahoo!' );
+         _.bindAll(this, 'render', 'additem');
+        this.render();
+    },
+    render: function(){
+        $(this.el).append("<button id ='add'>Say hello!</button>");
+    },
+    additem: function(){
+        $(this.el).append("<h1>Hello World!</h1>");
     }
   });
 
